e3ca527cbae388823a492c803c0b98173c15635b
Vihanga Rathnasekera <146312615+vihanga-isuru@users.noreply.github.com>
2024-06-15 03:19:03 +0000

    Copilot third recommit

--- comments.js
+++ comments.js
@@ -1,14 +1,31 @@
 //create web server
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
+const fs = require('fs');
 
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url === '/comments') {
+        fs.readFile('comments.js', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'application/javascript' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url === '/style.css') {
+        fs.readFile('style.css', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.write(data);
+            res.end();
+        });
+    }
 });
 
-//start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
